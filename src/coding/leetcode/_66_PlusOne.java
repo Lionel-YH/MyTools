f3f6373008cd707e824e252278ade765fb098220
Leo <buxingzhexiong@sina.com>
2018-09-05 08:35:00 +0800

    增加LeetCode 66题

--- src/coding/leetcode/_66_PlusOne.java
+++ src/coding/leetcode/_66_PlusOne.java
@@ -22,6 +22,26 @@ public class _66_PlusOne {
     }
 
 
+    //leetcode reference
+    public int[] plusOne_1(int[] digits) {
+
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+
+
     public static void main(String[] args){
         _66_PlusOne plusOne = new _66_PlusOne();
         int[] nums = { 9, 9};
